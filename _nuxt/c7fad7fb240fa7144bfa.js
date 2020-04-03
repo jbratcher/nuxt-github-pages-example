f0131502bbd803659d1bd762e32658e4d5898342
Jeremy Bratcher <jeremybratcher@gmail.com>
2020-04-03 12:09:43 -0400

    deploy

---
+++ _nuxt/c7fad7fb240fa7144bfa.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{187:function(t,e,n){"use strict";n.r(e);var l={},c=n(31),component=Object(c.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("h1",[this._v("This is the about page")])}),[],!1,null,null,null);e.default=component.exports}}]);
